a096920a3c3357e45d689dffa1789f6b98b5c3a0
A Dilbaro <a_dilbaro@outlook.com>
2023-03-21 12:26:43 +0000

    Fixed a bug

---
+++ prisma/migrations/20230321122616_another_migrateion/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `likes` on the `Comment` table. All the data in the column will be lost.
+  - You are about to drop the column `likes` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the `Likes` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Likes" DROP CONSTRAINT "Likes_postId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Likes" DROP CONSTRAINT "Likes_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "Comment" DROP COLUMN "likes";
+
+-- AlterTable
+ALTER TABLE "Post" DROP COLUMN "likes",
+ALTER COLUMN "title" SET DATA TYPE TEXT;
+
+-- DropTable
+DROP TABLE "Likes";
